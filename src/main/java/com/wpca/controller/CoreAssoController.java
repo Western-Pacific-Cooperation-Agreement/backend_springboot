653f72f79c13195add2725f26b4404ed6b6c8860
GitXulti <1449903582@qq.com>
2022-09-09 07:55:52 +0800

    锦坡同学实现了一个小功能

--- src/main/java/com/wpca/controller/CoreAssoController.java
+++ src/main/java/com/wpca/controller/CoreAssoController.java
@@ -1,9 +1,16 @@
 package com.wpca.controller;
 
 
+import com.wpca.common.lang.Result;
+import com.wpca.entity.CoreAsso;
+import com.wpca.service.CoreAssoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 /**
@@ -15,7 +22,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2022-09-04
  */
 @RestController
-@RequestMapping("/core-asso")
+@RequestMapping("/core")
 public class CoreAssoController extends BaseController {
+
+    @Autowired
+    CoreAssoService coreAssoService;
+
+    @GetMapping("/findAll")
+    public Result findAll(){
+        return Result.succ(coreAssoService.list());
+    }
 
 }
